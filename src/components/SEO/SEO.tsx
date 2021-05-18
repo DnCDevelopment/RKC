e27f59cb622a2de3dcb1f1b4c5371ca3f0749315
moon <mmesyatz@gmail.com>
2021-05-18 17:01:08 +0300

    fix: gtag

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -182,11 +182,20 @@ const SEO: React.FC<ISeoProps> = ({ article, breadcrumbs, description, lang, pat
       <script type="application/ld+json">{JSON.stringify(schemaOrgJSONLD)}</script>
       <script async src="https://www.googletagmanager.com/gtag/js?id=AW-368235489" />
       <script>{`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'AW-368235489');
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-368235489');
       `}</script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-WRCRVNLS9M" />
+      <script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-WRCRVNLS9M');
+      `}
+      </script>
     </Helmet>
   );
 };
