7b73268aabc9a8669df93c40fd3679a203ba3691
moon <mmesyatz@gmail.com>
2021-05-18 22:21:16 +0300

    fix: price label

--- src/templates/product.tsx
+++ src/templates/product.tsx
@@ -22,7 +22,7 @@ const Product: React.FC<IProductProps> = ({ data: { cockpitProduct } }): JSX.Ele
     link: { value: productLink },
     description: { value: description },
     price: { value: price },
-    measurment: { value: measurment },
+    measurment,
     images: { value: images },
     galleryTitle,
     galleryText,
@@ -97,7 +97,7 @@ const Product: React.FC<IProductProps> = ({ data: { cockpitProduct } }): JSX.Ele
       />
       <Subheader crumbs={productCrumbs} />
       <ProductBar />
-      <ProductInfo name={name} description={description} price={price} images={images} measurment={measurment} />
+      <ProductInfo name={name} description={description} price={price} images={images} measurment={measurment?.value} />
       {!!commonProducts?.value?.length && <ProductCommons products={commonProducts?.value} />}
       {specifications?.value?.data && <ProductSpecifications data={specifications.value.data} title={name} />}
       {galleryImages && (
