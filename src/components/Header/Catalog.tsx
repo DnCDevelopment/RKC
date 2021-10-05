38d4e217b50b3c8ddd7ff568ff76dabed41874b9
Dmitriy-sem <d.kazantsev11@gmail.com>
2021-10-05 11:02:49 +0300

    style: fix

--- src/components/Header/Catalog.tsx
+++ src/components/Header/Catalog.tsx
@@ -1,37 +1,42 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react'
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 
-import CatalogIcon from '../../assets/icons/catalog.svg'
-import Categories from './Categories'
-import SubCategories from './SubCategories'
+import CatalogIcon from '../../assets/icons/catalog.svg';
+import Categories from './Categories';
+import SubCategories from './SubCategories';
 
-import { ICatalogProps, ISubCategory } from './Types'
+import { ICatalogProps, ISubCategory } from './Types';
 
-import { TRANSLATE } from '../../constants/languages'
+import { TRANSLATE } from '../../constants/languages';
 
-import context from '../../context/context'
+import context from '../../context/context';
 
-import './Catalog.scss'
+import './Catalog.scss';
 
 const Catalog: React.FC<ICatalogProps> = ({ isMobile }): JSX.Element => {
-  const { language } = useContext(context)
+  const { language } = useContext(context);
 
-  const [category, setCategory] = useState<ISubCategory>()
-  const initialIsActiveValue =
-    typeof window !== 'undefined' && window.innerWidth >= 1030
-  const [isActive, setActive] = useState<boolean>(initialIsActiveValue)
+  const [category, setCategory] = useState<ISubCategory>();
+
+  const [isActive, setActive] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.innerWidth >= 1030) {
+      setActive(true);
+    }
+  }, []);
 
   const handleCatalogOpen = useCallback(() => {
-    setActive(!isActive)
+    setActive(!isActive);
     if (isMobile) {
-      document.body.classList.toggle('fixed')
+      document.body.classList.toggle('fixed');
     }
 
     if (!isActive) {
-      setCategory(undefined)
+      setCategory(undefined);
     } else if (isMobile) {
-      document.body.classList.add('fixed')
+      document.body.classList.add('fixed');
     }
-  }, [isActive, isMobile])
+  }, [isActive, isMobile]);
 
   return (
     <div className={`catalog-container ${isActive ? 'is-catalog-active' : ''}`}>
@@ -43,21 +48,10 @@ const Catalog: React.FC<ICatalogProps> = ({ isMobile }): JSX.Element => {
           {TRANSLATE[language as 'ru' | 'ua'].catalog}
         </div>
       </div>
-      {!!category && (
-        <SubCategories
-          isActive={isActive}
-          category={category?.subCategory || ''}
-          index={category?.index || 0}
-        />
-      )}
-      <Categories
-        isActive={isActive}
-        link={category?.subCategory || ''}
-        setActive={setActive}
-        setCategory={setCategory}
-      />
+      {!!category && <SubCategories isActive={isActive} category={category?.subCategory || ''} index={category?.index || 0} />}
+      <Categories isActive={isActive} link={category?.subCategory || ''} setActive={setActive} setCategory={setCategory} />
     </div>
-  )
-}
+  );
+};
 
-export default Catalog
+export default Catalog;
